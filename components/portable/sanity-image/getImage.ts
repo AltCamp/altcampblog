b381ea90c8d293fda1bd9ed22557abb37d21d95c
Samuel Adeboye Folaranmi <74235313+Boye95@users.noreply.github.com>
2023-09-05 17:04:52 +0100

    fix: wrong type for useNextSanityImage args

--- components/portable/sanity-image/getImage.ts
+++ components/portable/sanity-image/getImage.ts
@@ -1,7 +1,11 @@
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
-import client from '@/sanity/config/client-config'
+import config from '@/sanity/config/client-config'
 import { useNextSanityImage } from "next-sanity-image";
+import { createClient } from '@sanity/client';
+
+const configuredSanityClient = createClient(config);
+
 export function useImage(image: SanityImageSource) {
-  const imageProps = useNextSanityImage(client, image);
+  const imageProps = useNextSanityImage(configuredSanityClient, image);
   return imageProps;
 }
